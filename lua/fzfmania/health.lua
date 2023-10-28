6edbf49da0e1d5c9c28c19fcd97194f42be14fbe
Arsham Shirvani <arshamshirvani@gmail.com>
2023-10-28 11:29:53 +0100

    ref(health): directly call vim.health

--- lua/fzfmania/health.lua
+++ lua/fzfmania/health.lua
@@ -1,5 +1,4 @@
 local M = {}
-local health = vim.health
 
 local libs = {
   arshlib = "arsham/arshlib.nvim",
@@ -18,23 +17,23 @@ local executables = {
 }
 
 M.check = function()
-  health.report_start("FZFMania Health Check")
+  vim.health.start("FZFMania Health Check")
   for name, package in pairs(libs) do
     if not pcall(require, name) then
-      health.report_error(package .. " was not found", {
+      vim.health.error(package .. " was not found", {
         'Please install "' .. package .. '"',
       })
     else
-      health.report_ok(package .. " is installed")
+      vim.health.ok(package .. " is installed")
     end
   end
 
   for name, aspect in pairs(executables) do
     local ok = vim.fn.executable(aspect[1])
     if ok == 1 then
-      health.report_ok(name .. " is installed")
+      vim.health.ok(name .. " is installed")
     else
-      health.report_error(name .. " is not installed.", aspect[2])
+      vim.health.error(name .. " is not installed.", aspect[2])
     end
   end
 end
